16cb29aa72cf0357d6b09a900e8cc7a77919e5db
iuliabeldiman <dianabeldiman72@yahoo.ro>
2019-03-14 20:17:09 +0200

    add trip button

--- js/add-trip.js
+++ js/add-trip.js
@@ -1,39 +1,60 @@
-function addNewTrip(){
-    var myTopics = document.getElementsByClassName("my-topics")[0];
-    let newSpan = document.createElement("span");
-    
-    let newH4 = document.createElement('h4');
-    let newParagraph = document.createElement('p');
-    let newDiv = document.createElement('div');
-    let newButton1 = document.createElement('button');
-    let newButton2 = document.createElement('button');
-    
 
-//......modelare date
-    newH4.innerText = "Trip name din form"
-    newParagraph.innerText = "Date luate din form";
-    newButton1.innerText = "Deactivate";
-    newButton1.value = "on";
-    newButton2.innerText = "Delete";
-    
-    
-    
-//pana aicisa    
-    newSpan.appendChild(newH4);
-    newSpan.appendChild(newParagraph);
-    newSpan.appendChild(newDiv);
-    newDiv.appendChild(newButton1);
-    newDiv.appendChild(newButton2);
-    myTopics.appendChild(newSpan);
-    
-    newSpan.classList.add("topics");
-    newDiv.classList.add("topic-buttons-div");
-    newButton1.classList.add("active-inactive-topic");
-    newButton2.classList.add("delete-topic");
+function validateTripForm(){
 
-    console.log("end of function");
+    return true;
 }
 
-var form = document.getElementById("new-topic-form");
-form.addEventListener('submit', addNewTrip);
-//window.onload = addNewTrip;
+function addNewTrip(event){
+    var myTopics = document.getElementsByClassName("my-topics")[0];
+    var submitOk;
+    
+    submitOk = validateTripForm();
+    if(submitOk == false){
+        event.preventDefault();
+        return false;
+    }
+    else if(submitOk == true){
+        let newSpan = document.createElement("span");
+        let newH4 = document.createElement('h4');
+        let newParagraph = document.createElement('p');
+        let newDiv = document.createElement('div');
+        let newButton1 = document.createElement('button');
+        let newButton2 = document.createElement('button');
+        
+        //......modelare date + verificare
+        newH4.innerText = "Trip name din form"
+        newParagraph.innerText = "Date luate din form";
+        newButton1.innerText = "Deactivate";
+        newButton1.value = "on";
+        newButton2.innerText = "Delete";
+        
+        //pana aicisa    
+        newSpan.appendChild(newH4);
+        newSpan.appendChild(newParagraph);
+        newSpan.appendChild(newDiv);
+        newDiv.appendChild(newButton1);
+        newDiv.appendChild(newButton2);
+        myTopics.appendChild(newSpan);
+        
+        newSpan.classList.add("topics");
+        newDiv.classList.add("topic-buttons-div");
+        newButton1.classList.add("active-inactive-topic");
+        newButton2.classList.add("delete-topic");
+        //scrie datele si in BD + porneste cautarea
+        
+        event.preventDefault();
+        return true;
+    }
+    
+}
+
+
+var tripForm = document.getElementById("new-topic-form");
+
+if(tripForm.addEventListener){
+    tripForm.addEventListener("submit", addNewTrip, false);  
+}
+else if(tripForm.attachEvent){
+    tripForm.attachEvent("onsubmit", addNewTrip);
+}
+
